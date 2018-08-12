fc9a6ce3e378b5e8caf6099850f68709b69e3064
deeadebayo <deeadebayo@gmail.com>
2018-08-11 22:06:34 -0400

    adding gulp tasks, setting up structure

---
+++ gulp/tasks/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+    pug = require('gulp-pug');
+
+// gulp.task('task-name', function() {
+//     // Stuff here
+//   });
+
+gulp.task('hello', function() {
+    console.log('hello');
+});
