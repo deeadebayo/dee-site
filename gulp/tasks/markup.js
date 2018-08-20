76627d5c8403a09604e43916bc7b770e844191d0
deeadebayo <deeadebayo@gmail.com>
2018-08-20 01:29:40 -0400

    more gulpfile editing (PostCSS plugins). Add BrowserSync later

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -2,46 +2,53 @@ var gulp = require('gulp'),
     pug = require('gulp-pug'),
     postcss = require('gulp-postcss'),
     pugLinter = require('gulp-pug-linter'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer'),
+    precss = require('precss');
 
 
 // var err = error('MY_CODE', 'there is an error on this line', {})
 
 // Set up an object with the path variables - use variables in functions
-// var devPaths = {
-//     base:
-//     img:
-//     tmpl:
-//     script:
-//     fonts:
-// };
+var devPaths = {
+    // base:
+    // img:
+    tmpl: './app/**/*.pug',
+    styles: './app/assets/scss/**/*.css'
+    // script:
+    // fonts:
+};
 
-// var webPaths = {
+var webPaths = {
 //     base:
 //     img:
-//     tmpl:
+    tmpl: './dist',
+    styles: './dist/public/css/'
 //     script:
 //     fonts:
-// };
+};
 
 gulp.task('pug', function () {
     console.log('Processed HTML');
-    return gulp.src('./app/**/*.pug')
+    return gulp.src(devPaths.tmpl)
     .pipe(pug({
         pretty: false,
     }))
     .pipe(pugLinter())
     .pipe(pugLinter.reporter('fail'))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(devPaths.tmpl));
 });
 
 gulp.task('css', function () {
     var plugins = [
-        //plugins and options
+        autoprefixer(),
+        precss(),
     ];
-    return gulp.src('./app/assets/scss/**/*.css')
+    
+    return gulp.src(devPaths.styles)
     .pipe(sourcemaps.init())
     .pipe(postcss(plugins))
+    .pipe(autoprefixer('last 3 versions'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/public/css/'));
+    .pipe(gulp.dest(webPaths.styles));
 });
