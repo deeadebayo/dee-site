db8a9fc5b2f9c76dffe65375f8fc66bf5472a5ba
deeadebayo <deeadebayo@gmail.com>
2018-10-14 19:56:59 -0400

    added footer animation

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,1 +1,27 @@
-// import $ from 'jquery';
+// All JS code - till I get down modules
+import $ from 'jquery';
+
+console.warn(`yes, we're still good`);
+
+
+// For the footer animation
+$(document).ready(($) => {
+    $(window).on('scroll', () => {
+
+        //ADD .TIGHT
+        if ($(window).scrollTop() + $(window).height() > $('.wrapper').outerHeight()) {
+            $('body').addClass('tight');
+        } else {
+            $('body').removeClass('tight');
+        }
+    });
+
+    //BACK TO PRESENTATION MODE
+    $('html').on('click', 'body.tight .wrapper', () => {
+        $('html, body').animate({
+            scrollTop: $('.wrapper').outerHeight() - $(window).height()
+        }, 500);
+    });
+});
+
+console.warn(`still good`);
--- app/assets/js/modules/Footer.js
+++ app/assets/js/modules/Footer.js
@@ -1,20 +1,20 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
-export $(document).ready(($) => {
-    $(window).on('scroll', () => {
+// export $(document).ready(($) => {
+//     $(window).on('scroll', () => {
 
-        //ADD .TIGHT
-        if ($(window).scrollTop() + $(window).height() > $('.page-wrapper').outerHeight()) {
-            $('body').addClass('tight');
-        } else {
-            $('body').removeClass('tight');
-        }
-    });
+//         //ADD .TIGHT
+//         if ($(window).scrollTop() + $(window).height() > $('.page-wrapper').outerHeight()) {
+//             $('body').addClass('tight');
+//         } else {
+//             $('body').removeClass('tight');
+//         }
+//     });
 
-    //BACK TO PRESENTATION MODE
-    $('html').on('click', 'body.tight .wrapper', () => {
-        $('html, body').animate({
-            scrollTop: $('.wrapper').outerHeight() - $(window).height()
-        }, 500);
-    });
-});
+//     //BACK TO PRESENTATION MODE
+//     $('html').on('click', 'body.tight .wrapper', () => {
+//         $('html, body').animate({
+//             scrollTop: $('.wrapper').outerHeight() - $(window).height()
+//         }, 500);
+//     });
+// });
