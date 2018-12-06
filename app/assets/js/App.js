cdff7826fd92a43716d730c4f9b57660e185a281
Dee Adebayo <deeadebayo@gmail.com>
2018-12-05 19:03:43 -0500

    work - finished hello animation, just fix styles

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -3,54 +3,50 @@ import $ from 'jquery';
 
 console.warn(`yes, we're still good`);
 
+/** For the footer animation */
+$(document).ready($ => {
+	$(window).on('scroll', () => {
+		//ADD .TIGHT
+		if (
+			$(window).scrollTop() + $(window).height() >
+			$('.wrapper').outerHeight()
+		) {
+			$('body').addClass('tight');
+		} else {
+			$('body').removeClass('tight');
+		}
+	});
 
-// For the footer animation
-$(document).ready(($) => {
-    $(window).on('scroll', () => {
-
-        //ADD .TIGHT
-        if ($(window).scrollTop() + $(window).height() > $('.wrapper').outerHeight()) {
-            $('body').addClass('tight');
-        } else {
-            $('body').removeClass('tight');
-        }
-    });
-
-    //BACK TO PRESENTATION MODE
-    $('html').on('click', 'body.tight .wrapper', () => {
-        $('html, body').animate({
-            scrollTop: $('.wrapper').outerHeight() - $(window).height()
-        }, 500);
-    });
+	//BACK TO PRESENTATION MODE
+	$('html').on('click', 'body.tight .wrapper', () => {
+		$('html, body').animate(
+			{
+				scrollTop: $('.wrapper').outerHeight() - $(window).height()
+			},
+			500
+		);
+	});
 });
 
-console.warn(`still good`);
+/** Scroll window to an element  */
+$('a[href^="#"]')
+	.not('a[href="#"]')
+	.click(function(e) {
+		e.preventDefault();
+		$('html, body').animate(
+			{
+				scrollTop: $($(this).attr('href')).offset().top
+			},
+			500
+		);
+	});
 
-
-// For button sex
-$(function() {  
-    $('.btn-6')
-      .on('mouseenter', function(e) {
-              var parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-              $(this).find('span').css({top:relY, left:relX})
-      })
-      .on('mouseout', function(e) {
-              var parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-          $(this).find('span').css({top:relY, left:relX})
-      });
-    $('[href=#]').click(function(){return false});
-  });
-
-  /**
- *  Scroll window to an element
- */
-$('a[href^="#"]').not('a[href="#"]').click(function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-    }, 500);
+//Hello animation for homepage
+$(document).ready(() => {
+	let hello = document.getElementsByClassName('hello-animation');
+	let helloAction = $('ul.hello-animation__hellos');
+	$(hello).hover(() => {
+		helloAction.toggleClass('hello-animation__hellos--action');
+    });
+    console.warn('testing');
 });
