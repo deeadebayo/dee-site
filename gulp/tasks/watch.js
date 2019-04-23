84018a7a05d86db8270fe13a8b10d9dfc4b7d829
Dee Adebayo <deeadebayo@gmail.com>
2019-04-23 02:32:29 -0400

    watch task not functioning - debug on next commit

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,66 +1,61 @@
-const {watch, task, src, series, parallel}= require('gulp'),
-    // watch = require('gulp-watch'),
-    browserSync = require('browser-sync').create(),
-    reload = browserSync.reload,
+const { watch, task, src, series, parallel } = require('gulp'),
+	// watch = require('gulp-watch'),
+	browserSync = require('browser-sync').create(),
+	reload = browserSync.reload,
+	// Path/Directory variables
+	rawDir = {
+		img: './app/assets/img/*',
+		html: './app/*.pug',
+		styleFile: './app/assets/scss/styles.scss',
+		styles: './app/assets/scss/**/*.scss',
+		scripts: './app/assets/js/**/*.js'
+	},
+	serveDir = {
+		// img: './dist/public/img/',
+		html: './dist',
+		styleFile: '/dist/public/css/**/*.css',
+		styles: './dist/public/css/'
+		//scripts: './dist/public/js/'
+	};
 
-    // Path/Directory variables
-    rawDir = {
-        img: './app/assets/img/*',
-        html: './app/*.pug',
-        styleFile: './app/assets/scss/styles.scss',
-        styles: './app/assets/scss/**/*.scss',
-        scripts: './app/assets/js/**/*.js'
-    },
+function scriptsRefresh(cb) {
+	series('scriptTask', reload);
+	cb();
+}
 
-    serveDir = {
-        // img: './dist/public/img/',
-        html: './dist',
-        styleFile: '/dist/public/css/styles.css',
-        styles: './dist/public/css/',
-        //scripts: './dist/public/js/'
-    };
+function cssInject() {
+	return src(serveDir.styleFile).pipe(browserSync.stream());
+}
+
+function reloadPage(cb) {
+	series('compilePug', reload);
+	cb();
+}
 
 task('watch', () => {
-    browserSync.init({
-        open: false,
-        injectChanges: true,
-        server: {
-            baseDir: 'dist'
-        }
-    });
+	browserSync.init({
+		open: false,
+		injectChanges: true,
+		server: {
+			baseDir: 'dist'
+		}
+	});
 
-    watch(rawDir.html, () => {
-        series('compilePug');
-        reload();
-    });
+	watch(rawDir.html, reloadPage);
 
-    // watch(rawDir.styles, () => {
-    //     gulp.start('cssInject');
-    // });
-    watch(rawDir.styles, series('compileSass'));
-    
-    watch(rawDir.img, () => {
-        series('imgTask');
-        reload();
-    });
-    
-    watch(rawDir.scripts, () => {
-        series('scriptsRefresh');
-    });
+	/* watch(rawDir.html, function(cb) {
+		series('compilePug', reloadPage);
+		cb();
+	}); */
+
+	watch(rawDir.styles, series(cssInject, 'compileSass'));
+
+	watch(rawDir.img, () => {
+		series('imgTask', reload);
+	});
+	watch(rawDir.scripts, scriptsRefresh);
 });
-
-task('cssInject', series('compileSass', () => {
-    return src(serveDir.styleFile)
-        .pipe(browserSync.stream());
-}));
-
-task('default', series(parallel('scriptTask', 'markupTask', 'imgTask'), 'watch', () => {
-    // eslint-disable-next-line no-console
-    console.log('Let the watch party begin!!');
-}));
 
 task('build', parallel('scriptTask', 'markupTask', 'imgTask'));
 
-task('scriptsRefresh', series('scriptTask', () => {
-    reload();
-}));
+task('default', series('build', 'watch'));
