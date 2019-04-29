5186795c33d000860ce60d587df9431a238b816b
Dee Adebayo <deeadebayo@gmail.com>
2019-04-28 23:58:10 -0400

    initial commit

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -42,11 +42,22 @@ $('a[href^="#"]')
 	});
 
 //Hello animation for homepage
-$(document).ready(() => {
+/* $(document).ready(() => {
 	let hello = document.getElementsByClassName('hello-animation');
 	let helloAction = $('ul.hello-animation__hellos');
 	$(hello).hover(() => {
 		helloAction.toggleClass('hello-animation__hellos--action');
     });
     console.warn('testing');
-});
+}); */
+const helloAnimation = () => {
+	let hello = document.getElementsByClassName('hello-animation');
+	let helloAction = document.querySelectorAll('ul.hello-animation__hellos');
+
+	hello.hover(() => {
+		helloAction.toggleClass('hello-animation__hellos--action');
+	});
+	console.warn('testing');
+};
+
+exports.helloAnimation = helloAnimation;
