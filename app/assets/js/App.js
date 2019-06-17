08de7cc6e520ef7ee8d69489cdb49c4b496fb664
Dee Adebayo <deeadebayo@gmail.com>
2019-06-17 16:15:13 -0400

    minor edits to switch branch

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,6 +1,5 @@
-console.warn(`yes, we're still good`);
+// console.warn(`yes, we're still good`);
 
-import $ from 'jquery';
 const clickScroll = require('./modules/ClickScroll'),
 	helloAnimation = require('./modules/HelloAnimation'),
 	footerAnimation = require('./modules/Footer');
