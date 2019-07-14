f484e21c824fa9ea3ccc8d51e48b87979197f01c
Dee Adebayo <deeadebayo@gmail.com>
2019-07-13 23:35:53 -0400

    removed jQuery dependency for this module

--- app/assets/js/modules/HelloAnimation.js
+++ app/assets/js/modules/HelloAnimation.js
@@ -1,10 +1,8 @@
-import $ from 'jquery';
-
 export function helloAnimation() {
 	let hello = document.getElementsByClassName('hello-animation')[0];
 	let helloAction = document.querySelector('ul.hello-animation__hellos');
 
 	hello.addEventListener('mouseover', () => {
-		$(helloAction).toggleClass('hello-animation__hellos--action');
+		helloAction.classList.toggle('hello-animation__hellos--action');
 	});
 }
