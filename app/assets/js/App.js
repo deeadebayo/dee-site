67178ba288e784c7f0e8d93c3308dd9d0ef4c389
Dee Adebayo <deeadebayo@gmail.com>
2019-07-22 03:50:14 -0400

    reduced height of logo and smoothed out hover animation timings. Created an array of available hover colors

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,5 +1,3 @@
-console.warn(`yes, we're still good`);
-
 import { footerAnimation } from './modules/Footer';
 import { clickScroll } from './modules/ClickScroll';
 import { helloAnimation } from './modules/HelloAnimation';
