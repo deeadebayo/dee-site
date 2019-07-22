efab6be62f1540950e5c41b5a4206998dc858ec1
Dee Adebayo <deeadebayo@gmail.com>
2019-07-22 03:58:47 -0400

    Add logo (#15)
    * added svg logo to page
    
    * minor updates - assigned .logo class to svg's parent element
    
    * finished css animation for hover behavior. Testing multiple colors on hover with JS.
    
    * build script still working with updated npm packages. JS hover function is throwing errors
    
    * corrected most of the animation timings. JS (for color applies) is firing correctly. Work on sizing next
    
    * added variables for static logo as well
    
    * reduced height of logo and smoothed out hover animation timings. Created an array of available hover colors

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,9 +1,9 @@
-console.warn(`yes, we're still good`);
-
 import { footerAnimation } from './modules/Footer';
 import { clickScroll } from './modules/ClickScroll';
 import { helloAnimation } from './modules/HelloAnimation';
+import { showRandomColor } from './modules/LogoRandomColor';
 
 footerAnimation();
 clickScroll();
 helloAnimation();
+showRandomColor();
