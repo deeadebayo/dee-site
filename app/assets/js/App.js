9b3c419be428695fcf74d43177fb1e74e1da4ff3
deeadebayo <deeadebayo@gmail.com>
2019-08-12 15:18:24 -0500

    minial barba additions. Set up home animation container and headers

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -2,10 +2,11 @@ import { footerAnimation } from './modules/Footer';
 import { clickScroll } from './modules/ClickScroll';
 import { helloAnimation } from './modules/HelloAnimation';
 import { showRandomColor } from './modules/LogoRandomColor';
-import { Swup } from 'swup';
+//import { barba } from './modules/Barba';
+// import { Swup } from 'swup';
 
 new footerAnimation();
 new clickScroll();
 new helloAnimation();
 new showRandomColor();
-new Swup;
+// new Swup;
---
+++ app/assets/js/modules/Barba.js
@@ -0,0 +1,9 @@
+// import { barba } from "@barba/core";
+
+// export function barba.init(<options></options>) {
+//     transitions: [{
+//         enter({
+
+//         })
+//     }]
+// }
