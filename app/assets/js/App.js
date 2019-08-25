1c2c9bd13afb725a71ff744583575af175df6678
Dee Adebayo <deeadebayo@gmail.com>
2019-08-25 01:22:27 -0400

    switched pjax library from Barba to Highway

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,10 +1,11 @@
+import { myWay } from './modules/Highway';
 import { footerAnimation } from './modules/Footer';
 import { clickScroll } from './modules/ClickScroll';
 import { helloAnimation } from './modules/HelloAnimation';
 import { showRandomColor } from './modules/LogoRandomColor';
-require('./modules/Barba');
 
 new footerAnimation();
 new clickScroll();
 new showRandomColor();
 new helloAnimation();
+myWay();
