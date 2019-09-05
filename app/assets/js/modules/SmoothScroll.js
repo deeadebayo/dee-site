1050a5d5d421a3f154431d255228e074fa0fa06a
Dee Adebayo <deeadebayo@gmail.com>
2019-09-05 17:45:44 -0400

    init

--- app/assets/js/modules/SmoothScroll.js
+++ app/assets/js/modules/SmoothScroll.js
@@ -11,8 +11,14 @@ export function footerScroll() {
 	// 		);
 	// 	});
 	const footer = document.querySelector('#footer'),
-		navlink = document.querySelector('#contact-link');
+		navlink = document.querySelector('#contact-link'),
+		anchor = document.querySelectorAll('a[href^="#"]'),
+		
+	
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault();
 
+		})
 	navlink.addEventListener('click', () => {
 		footer.scrollIntoView({ behavior: 'smooth' });
 	});
