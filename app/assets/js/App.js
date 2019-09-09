617eb4def8ff9088fb8033c02e4a04f1bb8d6ebf
Dee Adebayo <deeadebayo@gmail.com>
2019-09-09 18:01:03 -0400

    deleted smoothscroll file

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,11 +1,9 @@
 import { footerAnimation } from './modules/Footer';
-// import { smoothScroll } from './modules/SmoothScroll';
 import { helloAnimation } from './modules/HelloAnimation';
 import { showRandomColor } from './modules/LogoRandomColor';
 // eslint-disable-next-line no-unused-vars
 import { pageAnimation } from './modules/PageAnimation';
 
-// new smoothScroll();
 new footerAnimation();
 new showRandomColor();
 new helloAnimation();
