16c6fc5a9d289b36feddd38044b7036975757678
Dee Adebayo <deeadebayo@gmail.com>
2019-09-09 18:01:03 -0400

    removed jquery smoothscroll module, changed scroll anchor links to not triger Highway

--- app/assets/js/modules/SmoothScroll.js
+++ app/assets/js/modules/SmoothScroll.js
@@ -1,28 +1,12 @@
+import $ from 'jquery';
+
 export function smoothScroll() {
-	// $('a[href^="#"]')
-	// 	.not('a[href="#"]')
-	// 	.click(function(e) {
-	// 		e.preventDefault();
-	// 		$('html, body').animate(
-	// 			{
-	// 				scrollTop: $($(this).attr('href')).offset().top
-	// 			},
-	// 			500
-	// 		);
-	// 	});
-	// const footer = document.querySelector('#footer'),
-	// navlink = document.querySelector('#contact-link'),
-	const anchor = document.querySelectorAll('a[href^="#"]', '!a[href^="#"]');
-	// body = document.body,
-	// wrapper = document.querySelectorAll('.wrapper'),
-	// html = document.documentElement;
-
-	anchor.addEventListener('click', function(e) {
-		e.preventDefault();
-		this.scrollIntoView({ behavior: 'smooth' });
-	});
-
-	// navlink.addEventListener('click', () => {
-	// 	footer.scrollIntoView({ behavior: 'smooth' });
-	// });
+	$('a[href^="#"]')
+		.not('a[href="#"]')
+		.click(function(e) {
+			e.preventDefault();
+			window.scroll({
+				top: $(this.getAttribute('href')).offset().top
+			});
+		});
 }
