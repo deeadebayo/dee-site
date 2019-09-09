6c321294298431974bdf443fefe1d7902af5ecd0
Dee Adebayo <deeadebayo@gmail.com>
2019-09-09 16:44:42 -0400

    updated the footer animation for es6. Removed jquery dependency

--- app/assets/js/modules/Footer.js
+++ app/assets/js/modules/Footer.js
@@ -1,29 +1,23 @@
-import $ from 'jquery';
-
 export function footerAnimation() {
 	const body = document.body,
-		wrapper = document.querySelectorAll('.wrapper'),
-		html = document.documentElement; //the html tag,
+		wrapper = document.querySelector('.wrapper'),
+		// tightBody = document.querySelector('body.tight'),
+		html = document.documentElement; //the html tag;
 
 	window.addEventListener('scroll', () => {
 		//ADD .TIGHT
-		if (
-			$(window).scrollTop() + $(window).height() >
-			$(wrapper).outerHeight()
-		) {
+		if (window.scrollY + window.innerHeight > wrapper.offsetHeight) {
 			body.classList.add('tight');
 		} else {
 			body.classList.remove('tight');
 		}
 	});
 
-	//BACK TO PRESENTATION MODE
-	$(html).on('click', 'body.tight .wrapper', () => {
-		$(html, body).animate(
-			{
-				scrollTop: $(wrapper).outerHeight() - $(window).height()
-			},
-			500
-		);
+	html.addEventListener('click', () => {
+		if (body.classList.contains('tight')) {
+			window.scroll({
+				top: wrapper.offsetHeight - window.innerHeight
+			});
+		}
 	});
 }
