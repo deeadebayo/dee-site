45b216fdcc0c594391659a31b0c01d222d0b9ecc
Dee Adebayo <deeadebayo@gmail.com>
2019-09-09 17:09:50 -0500

    Es smooth (#19)
    * init
    
    * blank
    
    * updated the footer animation for es6. Removed jquery dependency
    
    * footer - presentation mode was accepting all clicks to html. Now, only .wrapper
    
    * removed jquery smoothscroll module, changed scroll anchor links to not triger Highway
    
    * deleted smoothscroll file
    
    * deleted smoothscroll file

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,11 +1,9 @@
 import { footerAnimation } from './modules/Footer';
-import { footerScroll } from './modules/SmoothScroll';
 import { helloAnimation } from './modules/HelloAnimation';
 import { showRandomColor } from './modules/LogoRandomColor';
 // eslint-disable-next-line no-unused-vars
 import { pageAnimation } from './modules/PageAnimation';
 
 new footerAnimation();
-new footerScroll();
 new showRandomColor();
 new helloAnimation();
