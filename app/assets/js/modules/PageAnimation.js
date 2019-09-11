f991761779a9cf2b44105281665a1cffd12f1e65
Dee Adebayo <deeadebayo@gmail.com>
2019-09-10 20:25:35 -0400

    package updates. Removed commented out code

--- app/assets/js/modules/PageAnimation.js
+++ app/assets/js/modules/PageAnimation.js
@@ -9,18 +9,6 @@ const pageAnimation = new Highway.Core({
 
 if (Highway) {
 	console.warn(`it's working`);
-	// console.warn(real);
-	// links = document.querySelectorAll('.navbar__links a');
-	// // eslint-disable-next-line no-unused-vars
-	// pageAnimation.on('NAVIGATE_IN', ({ to, location }) => {
-	// 	for (let i = 0; i < links.length; i++) {
-	// 		const link = links[i];
-	// 		link.classList.remove('active');
-	// 		if (link.href === location.href) {
-	// 			link.classList.add('active');
-	// 		}
-	// 	}
-	// })
 } else {
 	console.warn(`Highway is not enabled`);
 }
