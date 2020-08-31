50f123c0b8b3f7d19b44051dc79f48cd20fe9dec
Dee Adebayo <deeadebayo@gmail.com>
2020-08-31 09:05:04 -0400

    experimenting with exit effects

--- app/assets/js/modules/highway-mods/transition__drawer.js
+++ app/assets/js/modules/highway-mods/transition__drawer.js
@@ -4,40 +4,37 @@ import anime from 'animejs/lib/anime.es';
 from: The data-router-view to hide.
 done: The required callback method that has to be called once the animation is done.
 
-in: This method should contain the animation to display a data-router-view.
-out: This method should contain the animation to hide a data-router-view. */
+in: This method should contain the animation to display a view.
+out: This method should contain the animation to hide a view. */
 
 //Drawer
 export default class Drawer extends Highway.Transition {
 	in({ from, to, done }) {
+		window.scrollTo(0, 0);
 		from.remove();
 
-		let loader = document.querySelector('#loader');
-
-		window.scrollTo(0, 0);
-
-		let timelineIn = anime.timeline({
-			targets: to,
-			opacity: [0, 1],
-			duration: 400,
-			easing: 'linear',
-		});
-		console.warn(`yes, i'm here`);
+		let loaderPage = document.querySelector('#loader'),
+			timelineIn = anime.timeline({
+				targets: to,
+				opacity: [0, 1],
+				duration: 400,
+				easing: 'linear',
+			});
 		timelineIn.add({
-			targets: loader,
-			top: ['0', '-100vh'],
-			duration: 400,
+			targets: loaderPage,
+			top: ['0', '100vh'],
+			duration: 500,
 			easing: 'easeOutExpo',
 		});
 
 		done();
 	}
 	out({ from, done }) {
-		let loader = document.querySelector('#loader'),
+		let loaderPage = document.querySelector('#loader'),
 			timelineOut = anime.timeline({
-				targets: loader,
+				targets: loaderPage,
 				top: ['-100vh', '0vh'],
-				duration: 400,
+				duration: 500,
 				easing: 'easeOutExpo',
 			});
 		timelineOut.add({
