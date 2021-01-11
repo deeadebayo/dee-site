8ea3fef9ee670f1de8850e9d69f3e8aa04837596
Dee Adebayo <deeadebayo@gmail.com>
2021-01-10 23:33:55 -0500

    fixed issue with eslint, starting slate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  siteMetadata: {
-    title: "dee-site",
-  },
-  plugins: [
-    "gatsby-plugin-emotion",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		title: 'dee-site',
+	},
+	plugins: [
+		'gatsby-plugin-emotion',
+		'gatsby-plugin-sharp',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sitemap',
+		'gatsby-plugin-offline',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png',
+			},
+		},
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: './src/images/',
+			},
+			__key: 'images',
+		},
+	],
 };
